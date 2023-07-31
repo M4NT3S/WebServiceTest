c51647b2d8d614a5d97d2279e299024a765af529
sebastian <mantes.dev@gmail.com>
2023-07-31 11:36:46 +0200

    Added new entity

---
+++ src/main/java/com/personal/webapp/SalutiWebService/entity/User.java
@@ -0,0 +1,16 @@
+package com.personal.webapp.SalutiWebService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private String surname;
+}
